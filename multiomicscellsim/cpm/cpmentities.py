[Fix] Pydantic computed_field preventing update of perimeter, leading to wrong calculation

multiomicscellsim/cpm/cpmentities.py
@@ -26,6 +26,7 @@ class CPMCell(BaseModel):
     id: int = 0
     cell_type: CPMCellType
     volume: int
+    perimeter: Optional[int] = Field(default=0)
     _neighbors: Dict[Tuple[int, int], List[Tuple[int, int]]] = {}
     
     @cached_property
@@ -33,7 +34,15 @@ class CPMCell(BaseModel):
         cols = ['step', 'id', 'type_id', 'volume', 'perimeter']
         return pd.DataFrame({c: [] for c in cols})
 
-    def log(self, step):
+    @model_validator(mode="after")
+    def init_perimeter(self) -> 'CPMCell':
+        """
+            This allows the perimeter to be set to a default value but not 
+        """
+        self.perimeter = self._compute_perimeter()
+        return self
+
+    def log(self, step) -> pd.DataFrame:
 
         new_data = {
                     'step': [step],
@@ -47,24 +56,13 @@ class CPMCell(BaseModel):
 
     def set_neighbors(self, neighbors: Dict[Tuple[int, int], List[Tuple[int, int]]]):
         self._neighbors = neighbors
-        self._perimeter = self._compute_perimeter()
+        self.perimeter = self._compute_perimeter()
 
     def gain_volume(self, gain: int = 1):
         self.volume += gain
 
     def _is_nb_diagonal(self, px, nb):
         return  (abs(px[0] - nb[0]) == 1) and (abs(px[1] - nb[1]) == 1)
-
-
-    @computed_field
-    @cached_property
-    def perimeter(self) -> int:
-        """
-        Perimeter. Value is computed on the first access then cached.
-        It is kept updated by neighbor setter and getters to avoid recomputing it every time
-        """
-        return self._compute_perimeter()
-
     
     def _compute_perimeter(self) -> int:
         #perim = 0
@@ -72,7 +70,7 @@ class CPMCell(BaseModel):
         #    # Count only neighbors that are "von_neumann"
         #    perim += np.sum( [1 for nb in nbs if not self._is_nb_diagonal(px, nb)])
         #return perim
-        return np.sum([nbs for src_coord, nbs in self._neighbors])
+        return np.sum([len(nbs) for src_coord, nbs in self._neighbors.items()])
     
     def add_neighbor_to(self, coords, neighbor):
         r, c = coords
@@ -80,7 +78,7 @@ class CPMCell(BaseModel):
             self._neighbors[(r, c)] = []
         if tuple(neighbor) not in self._neighbors[(r, c)]:
             self._neighbors[(r, c)].append(tuple(neighbor))
-            self._perimeter += 1
+            self.perimeter += 1
         else:
             # This is normal if we are dealing with more than two kinds of cell_id in the same nbhood
             pass
@@ -88,13 +86,13 @@ class CPMCell(BaseModel):
     def remove_neighbor_from(self, coords, neighbor, all=False):
         r, c = coords
         if all:
-            self._perimeter -= len(self._neighbors[(r, c)])
+            self.perimeter -= len(self._neighbors[(r, c)])
             del self._neighbors[(r,c)]
         else:
             if (r, c) in self._neighbors:
                 try:
                     self._neighbors[(r, c)].remove(tuple(neighbor))
-                    self._perimeter -= 1
+                    self.perimeter -= 1
                     # Clean up if no neighbors remain at (r, c)
                     if not self._neighbors[(r, c)]:
                         del self._neighbors[(r, c)]